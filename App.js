969fb88f0e278033b6ea7e8d2869767e1d3b7dd8
tomw <tom.winstanley@cdl.co.uk>
2021-01-31 23:01:20 +0000

    Login page, using axios to connect to remote server

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './components/Login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Login/>
     </View>
   );
 }
@@ -14,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#967259',
+    justifyContent: 'center'
   },
 });
